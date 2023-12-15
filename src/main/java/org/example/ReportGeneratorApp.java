eef9eb74a3db2ed95542fee453142660a053e60d
Mateusz Mroczkowski <nemomati1@gmail.com>
2023-12-15 21:12:23 +0100

    Parameters added

--- src/main/java/org/example/ReportGeneratorApp.java
+++ src/main/java/org/example/ReportGeneratorApp.java
@@ -15,25 +15,53 @@ import net.sf.jasperreports.engine.export.JRPdfExporterParameter;
 public class ReportGeneratorApp extends JFrame {
 
 	private JButton generateButton;
+	private JTextField imieField;
+	private JTextField nazwiskoField;
+	private JTextField opisField;
 
 	public ReportGeneratorApp() {
 		super("Report Generator");
 
 		initComponents();
 		setLayout(null);
-		setSize(300, 150);
+		setSize(400, 250);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 
 	private void initComponents() {
+		JLabel imieLabel = new JLabel("Name:");
+		imieLabel.setBounds(20, 30, 100, 30);
+
+		imieField = new JTextField();
+		imieField.setBounds(120, 30, 200, 30);
+
+		JLabel nazwiskoLabel = new JLabel("Last name:");
+		nazwiskoLabel.setBounds(20, 70, 100, 30);
+
+		nazwiskoField = new JTextField();
+		nazwiskoField.setBounds(120, 70, 200, 30);
+
+		JLabel opisLabel = new JLabel("Description:");
+		opisLabel.setBounds(20, 110, 100, 30);
+
+		opisField = new JTextField();
+		opisField.setBounds(120, 110, 200, 30);
+
 		generateButton = new JButton("Generate Report");
-		generateButton.setBounds(50, 30, 200, 50);
+		generateButton.setBounds(120, 150, 200, 30);
 		generateButton.addActionListener(e -> generateReport());
 
+		add(imieLabel);
+		add(imieField);
+		add(nazwiskoLabel);
+		add(nazwiskoField);
+		add(opisLabel);
+		add(opisField);
 		add(generateButton);
 	}
+
 
 	private Connection createConnection() throws SQLException {
 		String url = "jdbc:mysql://localhost:3306/biblioteka";
@@ -43,6 +71,12 @@ public class ReportGeneratorApp extends JFrame {
 	}
 
 	private void generateReport() {
+		// Check if fields are filled
+		if (imieField.getText().isEmpty() || nazwiskoField.getText().isEmpty() || opisField.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(this, "Please fill in all fields before generating the report.", "Error", JOptionPane.ERROR_MESSAGE);
+			return; // Exit the method if fields are not filled
+		}
+
 		Connection connection = null;
 
 		try {
@@ -52,11 +86,11 @@ public class ReportGeneratorApp extends JFrame {
 			// Load the report design
 			JasperReport jasperReport = JasperCompileManager.compileReport("/Users/mefunio/Documents/template/Raport_1.jrxml");
 
-			// Set parameters
+			// Set parameters from text fields
 			Map<String, Object> parameters = new HashMap<>();
-			parameters.put("Imie", "John");
-			parameters.put("Nazwisko", "Doe");
-			parameters.put("Opis", "Sample Report");
+			parameters.put("Imie", imieField.getText());
+			parameters.put("Nazwisko", nazwiskoField.getText());
+			parameters.put("Opis", opisField.getText());
 
 			// Create a JasperPrint object
 			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
@@ -83,6 +117,7 @@ public class ReportGeneratorApp extends JFrame {
 			}
 		}
 	}
+
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(ReportGeneratorApp::new);
